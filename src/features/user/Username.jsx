46c6409bdc20ecf82369f5615f7ef66e0fdf6441
Dusan Madjar <madjardusan@gmail.com>
2023-08-10 22:10:05 +0200

    Added style to Username

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Djarma</div>;
+  return <div className="hidden text-sm font-semibold md:block">Djarma</div>;
 }
 
 export default Username;
